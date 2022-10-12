33a3ea774b71086321b9c14224993f89078c0b2a
poojakrp777 <poojakrp777@gmail.com>
2022-10-12 15:42:48 +0530

    refactor-1 code

---
+++ FindMax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxfind_Generics
+{
+    internal class FindMax
+    {
+        public T FindMaxInteger<T>(T First, T Second, T Third) where T : IComparable
+        {
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
+            {
+                return First;
+            }
+            if (Second.CompareTo(First) > 0 && Second.CompareTo(Third) > 0)
+            {
+                return Second;
+            }
+            if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
+            {
+
+                return Third;
+            }
+            return default;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxfind_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Max Using Generics Program");
+
+            FindMax findMaximum = new FindMax();
+            Console.WriteLine("\nMaximum Integer value is : " + findMaximum.FindMaxInteger<int>(179, 586, 201));
+            Console.WriteLine("\nMaximum Floating value is : " + findMaximum.FindMaxInteger<double>(7.3, 6.5, 2.0));
+            Console.WriteLine("\nMaximum String is : " + findMaximum.FindMaxInteger<string>("Pooja", "Abhi", "Basava"));
+        }
+    }
+}
